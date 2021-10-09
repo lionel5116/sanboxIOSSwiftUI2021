77f8982f77a2952e339aa9e64ed0039af6cf0d88
lioneljones <lioneljones5116@gmail.com>
2021-10-08 23:41:24 -0500

    Initial Commit

---
+++ sanboxIOSSwiftUI2021/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  sanboxIOSSwiftUI2021
+//
+//  Created by lionel jones on 10/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ sanboxIOSSwiftUI2021/sanboxIOSSwiftUI2021App.swift
@@ -0,0 +1,17 @@
+//
+//  sanboxIOSSwiftUI2021App.swift
+//  sanboxIOSSwiftUI2021
+//
+//  Created by lionel jones on 10/8/21.
+//
+
+import SwiftUI
+
+@main
+struct sanboxIOSSwiftUI2021App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
